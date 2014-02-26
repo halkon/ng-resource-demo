d575dee1ce113d1ed47d83ce2b4f74ef54907b2b
Chris Cantu <christopher.cantu@gmail.com>
2014-02-26 11:23:32 -0600

    Fixed functional test

--- test/stories/validateHomePage.js
+++ test/stories/validateHomePage.js
@@ -1,17 +1,17 @@
 var homePage = require('../pages/HomePage'),
     expect = require('../setupExpect').expect;
 
-describe('LoginPage: Validate page content', function () {
+describe('HomePage: Validate page content', function () {
     before(function () {
         homePage.go();
     });
 
     it('Should display heading with correct content', function () {
         expect(homePage.heading).to.eventually.exist;
-        expect(homePage.heading.getText()).to.eventually.eq('Hello, I am a home page');
+        expect(homePage.heading.getText()).to.eventually.eq('Hello Developer, I am a home page');
     });
 
     it('Should be display scope variable', function () {
-        expect(homePage.hello.getText()).to.eventually.eq('Hello, I am a home page');
+        expect(homePage.hello.getText()).to.eventually.eq('Hello Developer, I am a home page');
     });
 });
