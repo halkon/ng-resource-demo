07f634191ac053bb7eb304cfbb597c44351f6872
Chris Cantu <christopher.cantu@gmail.com>
2014-04-30 15:03:08 -0500

    feat(rxTokenInterceptor): Added interceptors to template

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('encoreApp', ['ngRoute', 'ngResource', 'encore.ui', 'encore.ui.rxModalAction', 'homeSvcs'])
-    .config(function ($routeProvider, $locationProvider) {
+    .config(function ($routeProvider, $locationProvider, $httpProvider) {
         $routeProvider
             .when('/home', {
                 templateUrl: 'views/home/home.html',
@@ -12,6 +12,8 @@ angular.module('encoreApp', ['ngRoute', 'ngResource', 'encore.ui', 'encore.ui.rx
             });
 
         $locationProvider.html5Mode(true).hashPrefix('!');
+        $httpProvider.interceptors.push('TokenInterceptor'); //Injects auth token id into api calls
+        $httpProvider.interceptors.push('UnauthorizedInterceptor'); //Redirects user to login page on 401
     }).run(function ($window, Auth, Environment) {
         var environment = Environment.get().name;
 
