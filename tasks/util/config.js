d99a7a0464eb08fef4dec16f79fbcb1d1b7e13fe
Freddy Knuth <freddy.knuth@rackspace.com>
2014-05-07 13:40:39 -0500

    Proxy for staging login for local development

--- tasks/util/config.js
+++ tasks/util/config.js
@@ -19,6 +19,21 @@ module.exports = {
     },
     defaultProxies: [
         {
+            context: '/login',
+            host: 'staging.en.core.rackspace.com',
+            port: 443,
+            https: true,
+            protocol: 'https',
+            xforward: true,
+            changeOrigin: false,
+            rewrite: {
+                // Routes all login dependencies
+                '^/login/*': '/login/',
+                // Route login to index to avoid redirects
+                '^/login/?$': '/login/index.html'
+            }
+        },
+        {
             context: '/' + appName,
             host: 'localhost',
             port: 9000,
