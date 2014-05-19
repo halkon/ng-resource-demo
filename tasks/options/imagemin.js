fcbc8cf185705e4c4825e06c94d0b3f0bc4b451c
Freddy Knuth <freddy.knuth@rackspace.com>
2014-05-19 17:33:58 -0500

    copy encore-ui images in order for them to show up when deployed

--- tasks/options/imagemin.js
+++ tasks/options/imagemin.js
@@ -2,7 +2,7 @@ var config = require('../util/config.js');
 
 module.exports = {
     options: {
-    	config: config
+        config: config
     },
     dist: {
         files: [{
@@ -10,6 +10,11 @@ module.exports = {
             cwd: '<%= imagemin.options.config.app %>/images',
             src: '**/*.{png,jpg,jpeg}',
             dest: '<%= imagemin.options.config.appDest %>/images'
+        }, {
+            expand: true,
+            cwd: '<%= imagemin.options.config.app %>/bower_components/encore-ui',
+            src: '**/*.{png,jpg,jpeg}',
+            dest: '<%= imagemin.options.config.appDest %>/styles'
         }]
     }
 };
