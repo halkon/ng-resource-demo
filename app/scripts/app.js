7b7706fa5cc82e67424b6e9ffb76dbec48ce8435
Freddy Knuth <freddy.knuth@rackspace.com>
2014-06-05 16:52:43 -0500

    fix to test issues

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -14,10 +14,13 @@ angular.module('encoreApp', ['ngRoute', 'ngResource', 'encore.ui', 'encore.ui.rx
         $locationProvider.html5Mode(true).hashPrefix('!');
         $httpProvider.interceptors.push('TokenInterceptor'); //Injects auth token id into api calls
         $httpProvider.interceptors.push('UnauthorizedInterceptor'); //Redirects user to login page on 401
-    }).run(function ($http, $window, Auth) {
-        if (!Auth.isAuthenticated()) {
-            $window.location = '/login?redirect=' + $window.location.pathname;
-        }
+    }).run(function ($rootScope, $http, $window, Auth) {
+        $rootScope.$on('$locationChangeStart', function () {
+            if (!Auth.isAuthenticated()) {
+                $window.location = '/login?redirect=' + $window.location.pathname;
+                return;
+            }
+        });
 
         // Forces JSON only
         $http.defaults.headers.common['Accept'] = 'application/json';
