898cc8f2c338ee701e08d639d6f347c94302be78
Freddy Knuth <freddy.knuth@rackspace.com>
2014-08-21 17:23:08 -0500

    fix(usemin): Explicitly list out asset dirs

--- tasks/options/usemin.js
+++ tasks/options/usemin.js
@@ -5,6 +5,12 @@ module.exports = {
     options: {
         config: config,
         dirs: ['<%= usemin.options.config.appDest %>'],
-        assetsDirs: ['dist', '<%= usemin.options.config.appDest %>']
+        assetsDirs: ['dist',
+                     '<%= usemin.options.config.appDest %>',
+                     '<%= usemin.options.config.appDest %>/images',
+                     '<%= usemin.options.config.appDest %>/styles',
+                     '<%= usemin.options.config.appDest %>/fonts',
+                     '<%= usemin.options.config.appDest %>/scripts',
+                     '<%= usemin.options.config.appDest %>/views']
     }
 };
