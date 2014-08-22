f5b3f7b23074c18d3345d305ca4d3dfec857a7e3
Jay Parlar <parlar@gmail.com>
2014-08-22 13:13:13 -0400

    feat(prePush): Disallow force pushes to master

--- tasks/options/shell.js
+++ tasks/options/shell.js
@@ -12,12 +12,5 @@ module.exports = {
             'git commit -m "Build: Committing auto-generated reports"',
             'git push origin master'
         ].join('&&')
-    },
-    hooks: {
-        command: [
-            'rm -f .git/hooks/pre-commit',
-            'cp tasks/git-hooks/pre-commit .git/hooks/',
-            'chmod u+x .git/hooks/pre-commit'
-        ].join(' && ')
     }
 };
