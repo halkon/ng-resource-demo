32532376e630ae5b36dca5e8d72e6ebb43a7956f
Josh Miller <notjoshmiller@gmail.com>
2014-10-07 14:18:39 -0500

    Update deps. Remove grunt tasks. Huge clean up on tasks and their dependencies

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -6,16 +6,15 @@ var uglify = require('gulp-uglify');
 var csso = require('gulp-csso');
 var rev = require('gulp-rev');
 var revReplace = require('gulp-rev-replace');
-var plumber = require('gulp-plumber');
 
 gulp.task('html', ['lint', 'templates', 'styles', 'wiredep'], function () {
     var ngFilter = filter('app/scripts/**/*.js');
     var vendorFilter = filter('app/bower_components/**/*.js');
     var cssFilter = filter('**/*.css');
+    var assets = useref.assets();
 
     return gulp.src('app/index.html')
-        .pipe(plumber())
-        .pipe(useref.assets({ searchPath: './app' }))
+        .pipe(assets)
         .pipe(ngFilter)
         .pipe(ngAnnotate())
         .pipe(ngFilter.restore())
@@ -26,7 +25,7 @@ gulp.task('html', ['lint', 'templates', 'styles', 'wiredep'], function () {
         .pipe(csso())
         .pipe(cssFilter.restore())
         .pipe(rev())
-        .pipe(useref.restore())
+        .pipe(assets.restore())
         .pipe(useref())
         .pipe(revReplace())
         .pipe(gulp.dest('./dist/' + global.appName));
