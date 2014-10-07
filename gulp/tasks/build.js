073eceec61a6fb70947ee7e5d1734183b651b7ad
Josh Miller <notjoshmiller@gmail.com>
2014-10-07 14:50:56 -0500

    Create new build tasks to ensure failure on unit test and linting errors

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -1,6 +1,6 @@
 var gulp = require('gulp');
 
-gulp.task('build', ['karma:single'], function () {
+gulp.task('build', ['karma:build'], function () {
     gulp.start('clean');
     gulp.start('html');
     gulp.start('images');
