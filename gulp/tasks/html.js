d84e57893c7778dbf08b7fa06fbd488a6fdf114d
Josh Miller <notjoshmiller@gmail.com>
2014-10-30 11:36:27 -0500

    fix(build): Replace gulp-filter with gulp-if and remove ng-annotate. Fixes #92
    chore(deps): Move gulp-if into dev deps

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -1,29 +1,18 @@
 var gulp = require('gulp');
-var filter = require('gulp-filter');
 var useref = require('gulp-useref');
-var ngAnnotate = require('gulp-ng-annotate');
+var gulpif = require('gulp-if');
 var uglify = require('gulp-uglify');
 var csso = require('gulp-csso');
 var rev = require('gulp-rev');
 var revReplace = require('gulp-rev-replace');
 
 gulp.task('html', ['templates', 'styles', 'wiredep:index', 'index', 'images'], function () {
-    var ngFilter = filter('app/scripts/**/*.js');
-    var vendorFilter = filter('app/bower_components/**/*.js');
-    var cssFilter = filter('**/*.css');
     var assets = useref.assets();
 
     return gulp.src('app/index.html')
         .pipe(assets)
-        .pipe(ngFilter)
-        .pipe(ngAnnotate())
-        .pipe(ngFilter.restore())
-        .pipe(vendorFilter)
-        .pipe(uglify())
-        .pipe(vendorFilter.restore())
-        .pipe(cssFilter)
-        .pipe(csso())
-        .pipe(cssFilter.restore())
+        .pipe(gulpif('*.js', uglify()))
+        .pipe(gulpif('*.css', csso()))
         .pipe(rev())
         .pipe(assets.restore())
         .pipe(useref())
