f5bdfa5eda09d4c0dd4f9fccd5041548b5a844bd
Droogans <droogans@gmail.com>
2015-02-25 17:48:28 -0600

    chore(npm): Update all deps

--- gulp/tasks/hooks.js
+++ gulp/tasks/hooks.js
@@ -3,5 +3,10 @@ var symlink = require('gulp-symlink');
 
 gulp.task('hooks', function () {
   return gulp.src('gulp/hooks/pre-commit')
-    .pipe(symlink('.git/hooks'));
+    .pipe(symlink(function () {
+        return new symlink.File({
+            cwd: process.cwd(),
+            path: '.git/hooks/pre-commit'
+        });
+    }, { force: true }));
 });//hooks
