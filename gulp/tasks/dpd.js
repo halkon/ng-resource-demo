57eb6790263b3d262efc23a2d69a8dc55a25d36f
Freddy Knuth <halkon@gmail.com>
2017-02-07 13:58:30 -0600

    Update dpd.js

--- gulp/tasks/dpd.js
+++ gulp/tasks/dpd.js
@@ -2,10 +2,18 @@ var deployd = require('deployd');
 
 var gulp = require('gulp');
 
+var defaultConfig = {
+    port: 2403
+};
+
+if (process.env.MONGO_URI) {
+    defaultConfig.db = {
+        connectionString: process.env.MONGO_URI
+    };
+}
+
 gulp.task('dpd', function (next) {
-    var dpd = deployd({
-        port: 2403
-    });
+    var dpd = deployd(defaultConfig);
 
     console.log(dpd);
 
